89e79ddf4f8779130fedb20d0b4cd645ee97719f
Sertac Ozercan <sozercan@gmail.com>
2025-12-21 00:08:56 -0800

    fix concurrency issue
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- Core/Services/Player/PlayerService.swift
+++ Core/Services/Player/PlayerService.swift
@@ -563,12 +563,12 @@ final class PlayerService: NSObject, PlayerServiceProtocol {
 
                 // Update service state
                 if let likeStatus = songData.likeStatus {
-                    currentTrackLikeStatus = likeStatus
+                    self.currentTrackLikeStatus = likeStatus
                 }
-                currentTrackInLibrary = songData.isInLibrary ?? false
-                currentTrackFeedbackTokens = songData.feedbackTokens
+                self.currentTrackInLibrary = songData.isInLibrary ?? false
+                self.currentTrackFeedbackTokens = songData.feedbackTokens
 
-                logger.info("Updated track metadata - inLibrary: \(currentTrackInLibrary), hasTokens: \(currentTrackFeedbackTokens != nil)")
+                logger.info("Updated track metadata - inLibrary: \(self.currentTrackInLibrary), hasTokens: \(self.currentTrackFeedbackTokens != nil)")
             }
         } catch {
             logger.warning("Failed to fetch song metadata: \(error.localizedDescription)")
