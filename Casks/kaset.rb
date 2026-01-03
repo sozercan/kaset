8412059dbb8164bbc28cb1c5db3a7fea63188934
Goooler <wangzongler@gmail.com>
2026-01-03 09:02:11 +0800

    Tweak release workflow for updating the Cask

--- Casks/kaset.rb
+++ Casks/kaset.rb
@@ -2,15 +2,19 @@ cask "kaset" do
   version "0.4.1"
   sha256 "e63d0d61bb6d0c2c5a61db54fd10606a1816b70e822c867588d0a301a5dd49b1"
 
-  url "https://github.com/sozercan/kaset/releases/download/v0.4.1/kaset-v0.4.1.dmg"
+  url "https://github.com/sozercan/kaset/releases/download/v#{version}/kaset-v#{version}.dmg"
   name "Kaset"
-  desc "Native macOS YouTube Music client"
+  desc "Native YouTube Music client"
   homepage "https://github.com/sozercan/kaset"
 
   auto_updates true
   depends_on macos: ">= :tahoe"
 
   app "Kaset.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/Kaset.app"], sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/Kaset",
